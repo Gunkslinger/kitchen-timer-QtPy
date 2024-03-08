dce465d6821bfb958c1154a833de5c35136031d5
fcf <fcf@fastmail.com>
2024-03-08 14:21:14 -0500

    Consolidated stuff into a function for readability

--- kitchen-timer-qt.py
+++ kitchen-timer-qt.py
@@ -20,60 +20,56 @@ class MainWindow(QMainWindow, Ui_MainWindow, QTimer):
         self.m = 0
         self.s = 0
         self.CountDownText = " "
-        #self.spinBoxHours.valueChanged.connect(self.spinH)
-        #self.spinBoxMinutes.valueChanged.connect(self.spinM)
-        #self.spinBoxSeconds.valueChanged.connect(self.spinS)
         self.toolButtonStartStop.setCheckable(True)
         self.toolButtonStartStop.clicked.connect(self.start_stop)
         self.count_down_timer = QTimer(self)
         self.count_down_timer.timeout.connect(self.update_timer)
+
+
+    def update_countdown_label(self):
+        self.CountDownText = f"{self.h:0>2}:{self.m:0>2}:{self.s:0>2}"
+        self.labelCountDown.setText(self.CountDownText)
     
+
     def start_stop(self):
         b = self.toolButtonStartStop.isChecked()
         if b is True:
             self.h = self.spinBoxHours.value()
             self.m = self.spinBoxMinutes.value()
             self.s = self.spinBoxSeconds.value()
-            self.CountDownText = f"{self.h:0>2}:{self.m:0>2}:{self.s:0>2}"
-            self.labelCountDown.setText(self.CountDownText)
-            print(self.labelCountDown.text())
+            self.update_countdown_label()
             self.count_down_timer.start(1000)
         else:
             self.h = self.m = self.s = 0
+            self.update_countdown_label()
             self.count_down_timer.stop()
 
 
     def update_timer(self):
-        self.CountDownText = f"{self.h:0>2}:{self.m:0>2}:{self.s:0>2}"
-        self.labelCountDown.setText(self.CountDownText)
+        self.update_countdown_label()
         if self.s > 0:
             self.s -= 1
-            self.CountDownText = f"{self.h:0>2}:{self.m:0>2}:{self.s:0>2}"
-            self.labelCountDown.setText(self.CountDownText)
+            self.update_countdown_label()
 
         elif self.m > 0:
             self.m -= 1
             self.s = 59
-            self.CountDownText = f"{self.h:0>2}:{self.m:0>2}:{self.s:0>2}"
-            self.labelCountDown.setText(self.CountDownText)
+            self.update_countdown_label()
 
         elif self.h > 0:
             self.h -= 1
             self.m = 59
             self.s = 59
-            self.CountDownText = f"{self.h:0>2}:{self.m:0>2}:{self.s:0>2}"
-            self.labelCountDown.setText(self.CountDownText)
+            self.update_countdown_label()
 
         if self.h + self.m + self.s == 0:  # It's the fiiinal countdownnnn!!
             self.toolButtonStartStop.setChecked(False)
-            self.CountDownText = f"{self.h:0>2}:{self.m:0>2}:{self.s:0>2}"
-            self.labelCountDown.setText(self.CountDownText)
+            self.update_countdown_label()
             self.start_stop()
             # ALERT GOES HERE
             subprocess.run(CMD, shell=False)
 
-        print(self.labelCountDown.text())
-
+# class MainWindow
 
 def main():
     app = QApplication(sys.argv)

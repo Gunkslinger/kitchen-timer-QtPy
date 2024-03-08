d0aab84787f092371bba79f481d89a8114c90712
fcf <fcf@fastmail.com>
2024-03-07 20:23:58 -0500

    Still not complete

--- kitchen-timer-qt.py
+++ kitchen-timer-qt.py
@@ -31,27 +31,10 @@ class MainWindow(QMainWindow, Ui_MainWindow): #Ui_MainWindow):
     def pressedit(self, n):
         print("Start/Stop PRESSED: ", self.toolButtonStartStop.isChecked())
 
-#w = QtWidgets.QMainWindow()
-# # Create a label and display it all together
-# mylabel = QtWidgets.QLabel(mywindow)
-# mylabel.setText('Hello, World!')
-# mylabel.setGeometry(QtCore.QRect(200, 200, 200, 200))
-#mywindow = Ui_MainWindow()
-#w.setObjectName("Test ui")
-#Ui_MainWindow().setupUi(w)
-#w.resize(360, 330)
 
-#w.show()
-# for c in w.centralWidget().children():
-#     if c.objectName() == "gridLayoutWidget":
-#         for cc in c.children():
-#             print(cc)
-# Enter Qt application main loop
-#sys.exit(app.exec())
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    app.setApplicationName("Calculon")
+    app.setApplicationName("QtPyTimer")
 
     window = MainWindow()
-    #window.show()
     app.exec_()
